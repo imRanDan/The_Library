81cd134727eac268f9ca481bf9b1a3df9ec9aa59
Danyal <68073783+imRanDan@users.noreply.github.com>
2021-12-02 13:49:33 -0500

    Changes to HTML and redid JS
    So it turns out the reason why the submit button wouldn't work is because there was an extra </form> button right beside where i first inserted the form and that the JS code didn't detect the other articles of information to be entered.

---
+++ app.js
@@ -0,0 +1,69 @@
+//Book Class: Represents a Book
+class Book{
+   constructor(title, author, pages) {
+      this.title = title;
+      this.author = author;
+      this.pages = pages;
+   }
+}
+
+
+//UI Class: Handle UI Tasks
+class UI {
+   static displayBooks() {
+      const StoredBooks = [
+         {
+            title: 'Book One',
+            author: 'John Doe',
+            pages: '75'
+         },
+         {
+            title: 'Book Two',
+            author: 'Jane Doe',
+            pages: '75' 
+         }
+      ];
+
+      const books = StoredBooks;
+
+      books.forEach((book) => UI.addBookToList(book));
+   }
+
+   static addBookToList(book) {
+      const list = document.querySelector('#book-list');
+
+      const row = document.createElement('tr');
+
+      row.innerHTML = `
+         <td>${book.title}</td>
+         <td>${book.author}</td>
+         <td>${book.pages}</td>
+         <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+      `;
+
+      list.appendChild(row);
+   }
+}
+
+// Store Class: Handles Storage
+
+//Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+//Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+   //Prevent default
+   e.preventDefault();
+   
+   //Get form values
+   const title = document.querySelector('#title').value; 
+   const author = document.querySelector('#author').value;
+   const pages = document.querySelector('#pages').value;
+
+   // Instatiate Book
+   const book = new Book(title, author, pages);
+
+   console.log(book)
+});
+
+//Event: Remove a Book
